f9f4814ea5073e89f2f779388f11e7d9052327f2
Myrmex <dfusi@hotmail.com>
2021-12-28 11:08:48 +0100

    Added CodQuiresPart

---
+++ Cadmus.Seed.Codicology.Parts.Test/CodQuiresPartSeederTest.cs
@@ -0,0 +1,50 @@
+﻿using Cadmus.Codicology.Parts;
+using Cadmus.Core;
+using Fusi.Tools.Config;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Cadmus.Seed.Codicology.Parts.Test
+{
+    public sealed class CodQuiresPartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static CodQuiresPartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void TypeHasTagAttribute()
+        {
+            Type t = typeof(CodQuiresPartSeeder);
+            TagAttribute? attr = t.GetTypeInfo().GetCustomAttribute<TagAttribute>();
+            Assert.NotNull(attr);
+            Assert.Equal("seed.it.vedph.codicology.quires", attr!.Tag);
+        }
+
+        [Fact]
+        public void Seed_Ok()
+        {
+            CodQuiresPartSeeder seeder = new CodQuiresPartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            CodQuiresPart? p = part as CodQuiresPart;
+            Assert.NotNull(p);
+
+            TestHelper.AssertPartMetadata(p!);
+
+            Assert.NotEmpty(p!.Quires);
+        }
+    }
+}
---
+++ Cadmus.Seed.Codicology.Parts/CodQuiresPartSeeder.cs
@@ -0,0 +1,57 @@
+﻿using Bogus;
+using Cadmus.Codicology.Parts;
+using Cadmus.Core;
+using Fusi.Tools.Config;
+using System;
+
+namespace Cadmus.Seed.Codicology.Parts
+{
+    /// <summary>
+    /// Manuscript's quires sequence description seeder.
+    /// Tag: <c>seed.it.vedph.codicology.quires</c>.
+    /// </summary>
+    /// <seealso cref="PartSeederBase" />
+    [Tag("seed.it.vedph.codicology.quires")]
+    public sealed class CodQuiresPartSeeder : PartSeederBase
+    {
+        /// <summary>
+        /// Creates and seeds a new part.
+        /// </summary>
+        /// <param name="item">The item this part should belong to.</param>
+        /// <param name="roleId">The optional part role ID.</param>
+        /// <param name="factory">The part seeder factory. This is used
+        /// for layer parts, which need to seed a set of fragments.</param>
+        /// <returns>A new part.</returns>
+        /// <exception cref="ArgumentNullException">item or factory</exception>
+        public override IPart GetPart(IItem item, string roleId,
+            PartSeederFactory factory)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            CodQuiresPart part = new CodQuiresPart();
+            SetPartMetadata(part, roleId, item);
+
+            short nr = 1;
+            string[] types = new[] { "t2", "t3", "t4" };
+
+            for (int n = 1; n <= Randomizer.Seed.Next(1, 3 + 1); n++)
+            {
+                part.Types.Add(types[Randomizer.Seed.Next(0, types.Length)]);
+                part.Quires.Add(new Faker<CodQuire>()
+                    .RuleFor(q => q.Tag, f => f.Lorem.Word())
+                    .RuleFor(q => q.StartNr, nr)
+                    .RuleFor(q => q.EndNr, (short)(nr + 3))
+                    .RuleFor(q => q.SheetCount, 4)
+                    .RuleFor(q => q.SheetDelta, f => f.Random.Short(0, 2 + 1))
+                    .RuleFor(q => q.Note, f => f.PickRandom(null, f.Lorem.Sentence(2, 3)))
+                    .Generate());
+                nr += 4;
+            }
+
+            return part;
+        }
+    }
+}
