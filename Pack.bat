cd554fd90e472980821df3e4c6db430db06e40f3
Myrmex <dfusi@hotmail.com>
2026-07-21 18:33:04 +0200

    Updated packages

--- Pack.bat
+++ Pack.bat
@@ -1,24 +1,24 @@
 @echo off
 echo BUILD Cadmus Codicology Packages
-del .\Cadmus.Codicology.Parts\bin\Debug\*.*nupkg
-del .\Cadmus.Seed.Codicology.Parts\bin\Debug\*.*nupkg
-del .\Cadmus.Codicology.Services\bin\Debug\*.*nupkg
-del .\Cadmus.Codicology.Graph\bin\Debug\*.*nupkg
+del .\Cadmus.Codicology.Parts\bin\Release\*.*nupkg
+del .\Cadmus.Seed.Codicology.Parts\bin\Release\*.*nupkg
+del .\Cadmus.Codicology.Services\bin\Release\*.*nupkg
+del .\Cadmus.Codicology.Graph\bin\Release\*.*nupkg
 
 cd .\Cadmus.Codicology.Graph
-dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 cd..
 
 cd .\Cadmus.Codicology.Parts
-dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 cd..
 
 cd .\Cadmus.Seed.Codicology.Parts
-dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 cd..
 
 cd .\Cadmus.Codicology.Services
-dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 cd..
 
 pause
--- Push.bat
+++ Push.bat
@@ -4,10 +4,10 @@ prompt
 set nu=C:\Exe\nuget.exe
 set src=-Source https://api.nuget.org/v3/index.json
 
-%nu% push .\Cadmus.Codicology.Graph\bin\Debug\*.nupkg %src% -SkipDuplicate
-%nu% push .\Cadmus.Codicology.Parts\bin\Debug\*.nupkg %src% -SkipDuplicate
-%nu% push .\Cadmus.Codicology.Services\bin\Debug\*.nupkg %src% -SkipDuplicate
-%nu% push .\Cadmus.Seed.Codicology.Parts\bin\Debug\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Codicology.Graph\bin\Release\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Codicology.Parts\bin\Release\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Codicology.Services\bin\Release\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Seed.Codicology.Parts\bin\Release\*.nupkg %src% -SkipDuplicate
 echo COMPLETED
 pause
 echo on
